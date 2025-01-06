d7afed8bdbcffeac237f45460819d9f95d5aaff9
lordzzz <lordzzz8@gmail.com>
2025-01-06 11:30:18 +0100

    actualizado

---
+++ Proyecto_1_Pomodoro/Proyecto_1_Pomodoro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proyecto_1_Pomodoro
+//
+//  Created by Raúl Gallego Alonso on 5/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
