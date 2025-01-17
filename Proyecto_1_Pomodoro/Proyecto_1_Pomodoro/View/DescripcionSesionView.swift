a916afab75a7392f058733d18ab62545961122f2
Raul Gallego <raulgallego79@icloud.com>
2025-01-17 10:06:11 +0100

    proyecto

---
+++ Proyecto_1_Pomodoro/Proyecto_1_Pomodoro/View/DescripcionSesionView.swift
@@ -0,0 +1,23 @@
+//
+//  DescripcionSesion.swift
+//  Proyecto_1_Pomodoro
+//
+//  Created by Alejandro Gabriel Soriano Palomino on 11/01/25.
+//
+
+import SwiftUI
+
+struct DescripcionSesionView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+            }
+            .navigationTitle("Descripción Sesión")
+        }
+    }
+}
+
+#Preview {
+    DescripcionSesionView()
+}
